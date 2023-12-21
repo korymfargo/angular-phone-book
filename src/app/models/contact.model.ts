9fbdc8f81bcc32f1c5c9bff1ee0f5fc12364ba75
Kory Fargo <korymfargo.dev@gmail.com>
2023-12-20 19:21:08 -0600

    contact list static

---
+++ src/app/models/contact.model.ts
@@ -0,0 +1,12 @@
+export interface Contact {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  dob: string;
+  gender: string;
+  education: string;
+  company: string;
+  experience: number;
+  package: number;
+}

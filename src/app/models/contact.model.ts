211eca6c0155e0dd4ef6830cadd373b8e41c6561
Kory Fargo <korymfargo.dev@gmail.com>
2023-12-21 17:43:23 -0600

    add service for api, show contacts from api

--- src/app/models/contact.model.ts
+++ src/app/models/contact.model.ts
@@ -1,12 +1,12 @@
 export interface Contact {
   id: number;
-  firstName: string;
-  lastName: string;
+  firstname: string;
+  lastname: string;
   email: string;
   dob: string;
   gender: string;
   education: string;
   company: string;
-  experience: number;
-  package: number;
+  exp: number;
+  pkg: number;
 }
